91e9adc6500ea52e57af91357adcc5bef943bfe5
Yanina Gallardo <yanina.gallardo@netexlearning.net>
2022-02-10 17:58:59 -0300

    ejercicios basicos

---
+++ yani-Clase-01.js
@@ -0,0 +1,28 @@
+"use strict"
+
+let numero1 = prompt('Ingrese un numero');
+let numero2 = prompt('Ingrese otro numero');
+
+
+let suma = parseInt(numero1) + parseInt(numero2);
+
+console.log(suma);
+
+//Ejercicio 1
+
+/**
+ * Pedir al usuario Nombre, apellido y edad 
+ * e imprimirlo por consola 
+ */
+
+
+//Ejercicio 2
+
+/**
+ * Luciana va al almacen y compra 5 productos
+ * como es media inutil. Hacer una calculadora que pida 
+ * el precio de los productos y muestre por consola el total. 
+ */
+
+
+
