ef8d129e3c235dd155b1d19434f95c3beb4846f2
hoba86 <jesistq@gmail.com>
2022-02-13 17:22:05 -0300

    ejercicio resuelto

--- jesi/clase-01.js
+++ jesi/clase-01.js
@@ -1,3 +1,5 @@
+"use strict"
+
 let num1= 20
 let num2=30
 let num3= 50
@@ -8,4 +10,15 @@ console.log (num1+num2+num3+num4);
 let apellido= prompt ('ingrese apellido')
 let nombre= prompt ('ingrese nombre')
 let edad= prompt ('ingrese edad')
- 
+
+console.log (nombre+apellido+edad)
+
+  
+let numero1= prompt ('ingrese 23') 
+let numero2= prompt ('ingrese 4')
+let numero3= prompt ('ingrese 2')
+let nuemro4= prompt ('ingrese 100')
+let nuemro5= prompt ('ingrese 3')
+
+
+console.log (numero1=3, numero2=100, numero3=2, nuemro4=4, nuemro5=23)
