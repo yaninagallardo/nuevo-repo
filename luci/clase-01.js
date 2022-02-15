07d58c44945b0f4364bcbd8cd6885066e2f5d542
luugallardo <lucianagallardo999@gmail.com>
2022-02-15 13:59:34 -0300

    ejercicio 1

---
+++ luci/clase-01.js
@@ -0,0 +1,21 @@
+let num1 = 75
+let num2 = 45
+let num3 = 110
+let num4 = 30
+let num5 = 60
+
+console.log (num1 + num2 + num3 + num4 + num5)
+
+let numero1= prompt ('Ingreso 23')
+let numero2= prompt ('Ingreso 4')
+let numero3= prompt ('Ingreso 2')
+let numero4= prompt ('Ingreso 100')
+let numero5= prompt ('Ingreso 3')
+
+console.log (numero5 + numero4 + numero3 + numero2 + numero1)
+
+let numero1 = prompt ("Nombre")
+let numero2 = prompt ("Apellido")
+let numero3 = prompt ("Edad")
+
+console.log (numero1 + numero2 + numero3)
