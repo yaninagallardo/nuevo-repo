cffd9e9329debbe78b3619270a52764718b6c3fd
Yanina Gallardo <yanina.gallardo@netexlearning.net>
2022-02-17 17:51:59 -0300

    ejemplo loguearse

--- yani/clase-02.js
+++ yani/clase-02.js
@@ -11,18 +11,40 @@
  * 
  */
 
-const minAge = 21;
-const maxAge = 35;
+// const minAge = 21;
+// const maxAge = 35;
 
-let userAge = prompt("Ingrese su edad");
+// let userAge = prompt("Ingrese su edad");
 
-if ( (userAge >= minAge) && (userAge <= maxAge) ){
-    console.log("Solo entra a la cafeteria");
-} else if ( userAge > maxAge){
-    console.log("Entra a zona vip");
+// if ( (userAge >= minAge) && (userAge <= maxAge) ){
+//     console.log("Solo entra a la cafeteria");
+// } else if ( userAge > maxAge){
+//     console.log("Entra a zona vip");
+// } else {
+//     console.log("No puede entrar");
+// }
+
+
+let userName = prompt("Ingrese usuario");
+let password = prompt("Ingrese contraseña");
+
+const user = "admin";
+const pass = "admin";
+
+if ( (userName === user) ) {
+    if( password === pass ) {
+        console.log("Entra");
+    } else {
+        console.log("contraseña incorrecta");
+    }
 } else {
-    console.log("No puede entrar");
+    console.log("el usuario no existe");
 }
 
+if ( (userName === user) && (password === pass) ) {
+    console.log("entro");
+} else{
+    console.log("el usuario o contraseña es incorrecto");
+}
 
 // Sigue con el codigo
